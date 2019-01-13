bea20f108b8bab29ea48b17aaa602f5d84322919
Tyrease Teer <46007223+HarashiGouse2019@users.noreply.github.com>
2019-01-12 22:06:48 -0700

    Basic Movement With Collision

---
+++ Sprite Mover/Assets/Player Movement/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveForce = 365F;
+    public float maxSpeed = 5F;
+    public Transform checkCollision;
+
+    private bool grounded = false;
+    private Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        grounded = Physics2D.Linecast(checkCollision.position, checkCollision.position, 1 << LayerMask.NameToLayer("Ground"));
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        }
+
+        if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        }
+    }
+}
